c84c6fe8de34544709dbb40653a8c2bdef62b923
ievgen <e.dilevskiy@gmail.com>
2021-04-13 14:46:43 -0600

    Add interviewerListItem component & stories

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -4,23 +4,24 @@ import classnames from "classnames/bind";
 import "components/InterviewerListItem.scss";
 
 export default function InterviewerListItem(props) {
-// const dayClass = classnames("day-list__item", {
-//   "day-list__item--selected": props.selected,
-//   "day-list__item--full": !props.spots
-// });
+  const interviewerClass = classnames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+    // "day-list__item--full": !props.spots
+  });
   // const formatSpots = () => {
   //   if (props.spots === 0) return `no spots remaining`;
   //   if (props.spots === 1) return ` ${props.spots} spot remaining`;
   //   return `${props.spots} spots remaining`
   // }
   return (
-    <li className="interviewers__item">
+    <li className={interviewerClass}
+    onClick={() => props.setInterviewer(props.name)}>
   <img
     className="interviewers__item-image"
     src={props.avatar}
     alt={props.name}
   />
-  {props.name}
+  {props.selected && props.name}
 </li>
   );
 }
