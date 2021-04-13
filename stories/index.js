b497eb9596d661dc56ca42c071dcea406b55d649
ievgen <e.dilevskiy@gmail.com>
2021-04-13 17:49:43 -0600

    Add Appointment components

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,13 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
+
+
 
 storiesOf("Button", module)
   .addParameters({
@@ -139,4 +146,9 @@ storiesOf("DayList", module)
           time="12pm"
         />
       ))
-      .add("Header", () => <Header time="12pm" />) // the rest of your code should go here
+      .add("Header", () => <Header time="12pm" />)
+      .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+      .add("Show", () => <Show onEdit={action("onEdit")} onDelete={action("onDelete")} />)
+      .add("Confirm", () => <Confirm onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
+      .add("Status", () => <Status message="Deleting" />)
+      .add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")} />)
