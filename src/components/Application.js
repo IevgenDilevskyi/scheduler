c8554276f5ebc21f266b8f27dcd3bd450c834414
ievgen <e.dilevskiy@gmail.com>
2021-04-14 21:02:07 -0600

    add change state after days & appointments are fetched

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,65 +5,88 @@ import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "3pm",
-  },
-  {
-    id: 4,
-    time: "4pm",
-    interview: {
-      student: "Cool guy",
-      interviewer: {
-        id: 5,
-        name: "Sven Jones",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4.30pm",
-    interview: {
-      student: "Eugene JD",
-      interviewer: {
-        id: 2,
-        name: "Tori Malcolm",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-];
+// const appointments = [
+//   {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 1,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   {
+//     id: 3,
+//     time: "3pm",
+//   },
+//   {
+//     id: 4,
+//     time: "4pm",
+//     interview: {
+//       student: "Cool guy",
+//       interviewer: {
+//         id: 5,
+//         name: "Sven Jones",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   {
+//     id: 5,
+//     time: "4.30pm",
+//     interview: {
+//       student: "Eugene JD",
+//       interviewer: {
+//         id: 2,
+//         name: "Tori Malcolm",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+// ];
 
 export default function Application(props) {
 
-  const [days, setDays] = useState([]);
-  const url = "http://localhost:8001/api/days";
+  // const [days, setDays] = useState([]);
+  // let [day, setDay] = useState('Monday');
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    // you may put the line below, but will have to remove/comment hardcoded appointments variable
+    appointments: {}
+  });
+
+  const dailyAppointments = [];
+
+  const setDay = day => setState({ ...state, day });
+  // const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
-    axios.get(url).then(response => {
-      setDays([...response.data]);
-    })}, []);
+    const daysPromise = axios.get("/api/days");
+    const appointmentsPromise = axios.get("/api/appointments");
+    const promises = [daysPromise,appointmentsPromise];
+    Promise.all(promises)    
+      .then(res => {
+        console.log("Promise.all RESPONSE",res);
+        setState(prev => ({...prev, days: res[0].data, appointments: res[1].data}))
+      })
+  }, []);
 
-  let [day, setDay] = useState('Monday');
-  const appointmentsList = appointments.map(appointment => {
+    // useEffect(() => {
+    //   axios.get("/api/days")
+      
+    //   .then(response => {
+    //     // setDays([...response.data]);
+    //   })}, []);
+
+  const appointmentsList = dailyAppointments.map(appointment => {
     return(
       <Appointment
         key={appointment.id}
@@ -85,8 +108,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
           <nav className="sidebar__menu">
             <DayList
-              days={days}
-              day={day}
+              days={state.days}
+              day={state.day}
               setDay={setDay}
             />
           </nav>
